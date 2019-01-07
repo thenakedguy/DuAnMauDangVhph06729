36c9cef7797b36c87c390dc6364ef9c399af9ab9
HngDng <vidang68@gmail.com>
2019-01-07 13:07:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/hngdng/duanmaudangvhph06729/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hngdng.duanmaudangvhph06729;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
